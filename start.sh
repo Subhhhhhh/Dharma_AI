c8d124cdf4e172b474ff5749734a37fbf9477cae
subhhhhhh <subhajeetkar449@gmail.com>
2025-12-16 15:46:23 +0530

    Clean Railway deployment

--- start.sh
+++ start.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-echo "Running migrations..."
-python manage.py migrate --noinput
-echo "Migrations done."
 
+python manage.py migrate
 python manage.py collectstatic --noinput
-
-gunicorn dharma_ai.wsgi:application \
-  --bind 0.0.0.0:$PORT \
-  --workers 2 \
-  --timeout 120
+gunicorn dharma_ai.wsgi:application --bind 0.0.0.0:$PORT
